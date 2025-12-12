dd7dddda54e19a32e6455b6f494140b09a9f3d4c
KarChikey <kartikeynegi2002@gmail.com>
2025-12-12 13:55:20 +0530

    add

--- backend/app/agent/react_agent.py
+++ backend/app/agent/react_agent.py
@@ -2,14 +2,12 @@ from langchain_experimental.agents import create_pandas_dataframe_agent
 from ..llm_loder.llm import load_llm
 from ..data.load_data import load_dataframe
 
-def run_react_agent(query:str, file_path:str="app/agent/sample_employees.csv"):
-    df=load_dataframe(file_path)
+def run_react_agent(query:str):
+    df = load_dataframe()
+
     llm=load_llm()
     agent=create_pandas_dataframe_agent(llm=llm,
                                         df=df,
                                         verbose=True,
                                         allow_dangerous_code=True)
     return agent.run(query)
-
-if __name__=="__main__":
-    print(run_react_agent("what is the average age of the people?"))
